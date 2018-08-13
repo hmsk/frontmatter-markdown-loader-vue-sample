442550c854302a9f42fd4350184909b2fc47cdd1
hmsk <k.hamasaki@gmail.com>
2018-08-13 02:56:29 -0700

    Configure frontmatter-markdown-loader

---
+++ vue.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  chainWebpack: config => {
+    config.module
+      .rule('markdown')
+      .test(/\.md$/)
+      .use('frontmatter-markdown-loader')
+        .loader('frontmatter-markdown-loader')
+        .end()
+  }
+}

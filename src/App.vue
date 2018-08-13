1ffeca7f8100b24385347c180c02ef97f76b070d
hmsk <k.hamasaki@gmail.com>
2018-08-13 10:04:24 -0700

    Remove provided initial component

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
     <Readme />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 import Readme from './components/Readme.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld, Readme
+    Readme
   }
 }
 </script>
